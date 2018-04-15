dfd47550139a1b557b7ff5588cc2d4e4e86dc385
vagrant <vagrant@localhost.localdomain>
2018-04-15 01:05:58 -0400

    Implement pgbackup full functionality

--- setup.py
+++ setup.py
@@ -10,7 +10,12 @@ setup(
     long_description=readme,
     author='Keith',
     author_email='kcarter241@gmail.com',
-    install_requires=[],
+    install_requires=['boto3'],
     packages=find_packages('src'),
-    package_dir={'': 'src'}
+    package_dir={'': 'src'},
+    entry_points = {
+        'console_scripts': [
+            'pgbackup=pgbackup.cli:main',
+            ]
+    }
 )
